e04192bfcbc39b190c59552b82483799cc6b524e
zamorano <bzam1204@gmail.com>
2024-07-11 17:55:48 -0400

    second commit

---
+++ app/login/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface Props {
+    // Define the props for your component here
+}
+
+const Page: React.FC<Props> = (props) => {
+    // Add your component logic here
+
+    return (
+        <div>
+            login
+        </div>
+    );
+};
+
+export default Page;
