f85292849b70a1338cb4100b84ad432d0b1c9318
Bruno Zamorano Alves Monteiro <bzam1204@gmail.com>
2024-08-10 23:21:40 -0400

    refactor: Update question card component styling and structure

--- app/ui/question-card-to-answer.tsx
+++ app/ui/question-card-to-answer.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useState } from "react";
 import { XMarkIcon } from "@heroicons/react/20/solid";
-import {  CheckIcon } from "@heroicons/react/24/outline";
+import { CheckIcon } from "@heroicons/react/24/outline";
 import { Question, QuestionType } from "@prisma/client";
 import { createOrUpdateFibonacciLog } from "../lib/actions";
 
@@ -11,23 +11,17 @@ interface Props {
 }
 
 
-const QuestionCardToAnswer: React.FC<Props> = ({ question, questionType }) => {
+const QuestionCardToAnswer: React.FC<Props> = ({ question }) => {
     const [result, setResult] = useState<Boolean | null>(null)
-
-    console.log('questão: ',question.body,'|| correct: ', question.correctAnswer)
     async function handleAnswerQuestion(givenAnswer: any, correctAnswer: any) {
-
         const result = givenAnswer === correctAnswer
-
         if (result === true) await createOrUpdateFibonacciLog(question.id, question.userId, result)
-
         return result;
     }
 
     return (
         <div
-            className="flex relative flex-row bg-transparent gap-4 w-full justify-between max-w-screen-md border-transparent border-x-8 border-black transition-all duration-200 hover:border-x-0 hover:border-gray-200 hover:bg-white p-4 hover:rounded-r hover:border-l-amber-400 hover:border-l-8 hover:pl-6 hover:drop-shadow-md"
-        >
+            className="flex relative flex-row bg-transparent gap-4 w-full justify-between max-w-screen-md border-transparent border-x-8 border-black transition-all duration-200 hover:border-x-0 hover:border-gray-200 hover:bg-white p-4 hover:rounded-r hover:border-l-amber-400 hover:border-l-8 hover:pl-6 hover:drop-shadow-md">
 
             <p>{question.body}</p>
             <div className='flex flex-col gap-3 justify-around'>
