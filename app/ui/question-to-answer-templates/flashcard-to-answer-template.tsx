636a927126da9c9db6c8f6f98c3796dff604ab01
Bruno Zamorano Alves Monteiro <bzam1204@gmail.com>
2024-08-13 21:50:11 -0400

    feat: Add FlashCardToAnswerTemplate component for rendering flashcard content

---
+++ app/ui/question-to-answer-templates/flashcard-to-answer-template.tsx
@@ -0,0 +1,44 @@
+'use client'
+import React from "react";
+import { useEditor, EditorContent } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import { useFormState } from "react-dom";
+import { createQuestion } from "@/app/lib/actions";
+
+
+const FlashCardToAnswerTemplate = ({frontContent, backContent}: {frontContent: string, backContent: string}) => {
+    const [state, formAction] = useFormState(createQuestion, undefined);
+
+
+    // Editor instance for rendering content
+    const frontEditor = useEditor({
+        extensions: [StarterKit],
+        content: frontContent,
+        editable: false, // Make it non-editable
+    });
+
+    const backEditor = useEditor({
+        extensions: [StarterKit],
+        content: backContent,
+        editable: false, // Make it non-editable
+    });
+
+    return (
+        <div>
+            {/* Apresentação do Conteúdo com TipTap */}
+            <div className="mt-8">
+                <h2 className="text-xl font-bold">Conteúdo do Cartão</h2>
+                <div className="mt-4">
+                    <h3 className="font-semibold">Frente:</h3>
+                    <EditorContent content={frontContent} editor={frontEditor} />
+                </div>
+                <div className="mt-4">
+                    <h3 className="font-semibold">Explicação:</h3>
+                    <EditorContent content={backContent} editor={backEditor} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FlashCardToAnswerTemplate;
