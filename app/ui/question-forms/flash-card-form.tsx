16f6382bb5267c1be72b18d7c4fdbf7540140c4d
Bruno Zamorano Alves Monteiro <bzam1204@gmail.com>
2024-08-13 21:49:51 -0400

    refactor: Update FlashCardForm component structure and add Tiptap text editor

--- app/ui/question-forms/flash-card-form.tsx
+++ app/ui/question-forms/flash-card-form.tsx
@@ -1,9 +1,38 @@
 import React from "react";
+import Tiptap from "../text-editor/tip-tap";
+import { Button } from "../button";
+import { SubmitButton } from "./summary-form";
+import { CirclePlus } from "lucide-react";
+import { useFormState, useFormStatus } from "react-dom";
+import { createQuestion } from "@/app/lib/actions";
+
+const SubmitBtn = () => {
+    const { pending } = useFormStatus();
+
+    return (
+        <SubmitButton Icon={CirclePlus} label="Criar Flashcard" _pending={pending} />
+    );
+};
 
 const FlashCardForm = () => {
+    const [state, formAction] = useFormState(createQuestion, undefined);
+    const [frontContent, setFrontContent] = React.useState('');
+    const [backContent, setBackContent] = React.useState('');
+  
     return (
-        <div>
-            <h1>FlashCardForm</h1>
+      <div>
+        <form action={formAction}>
+          <input hidden readOnly type="text" name="userId" value="clz3g43fz00049moixkx337j8" />
+          <input hidden readOnly type="text" name="typeId" value="c8021e52-0109-4c2f-bf9d-32817fcdf792" />
+          <input hidden type="text" name="body" value={frontContent} />
+          <input hidden type="text" name="explanation" value={backContent} />
+          <p>Frente do cartão</p>
+          <Tiptap onContentChange={setFrontContent} />
+          <p>Explicação do cartão</p>
+          <Tiptap onContentChange={setBackContent} />
+                <SubmitBtn />
+                {state?.message && <p className="mt-2 text-sm text-green-500">{state.message}</p>}
+            </form>
         </div>
     );
 };
