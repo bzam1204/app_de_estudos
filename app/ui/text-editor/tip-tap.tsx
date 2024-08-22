38c4cd45b125018f38acec0eaaf5fb0d513a6cee
Bruno Zamorano Alves Monteiro <bzam1204@gmail.com>
2024-08-22 07:28:31 -0400

    refactor: Update Tiptap import in FlashCardForm

--- app/ui/text-editor/tip-tap.tsx
+++ app/ui/text-editor/tip-tap.tsx
@@ -1,18 +1,15 @@
 'use client'
-
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 
 const Tiptap = ({ content, onContentChange }: { onContentChange: (prop: any) => void, content: string }) => {
   const editor = useEditor({
-    extensions: [StarterKit ],
-
+    extensions: [StarterKit],
     onUpdate: ({ editor }) => {
       const HTML = editor.getHTML()
       onContentChange(HTML)
     },
     content: content,
-    
   })
 
   return <EditorContent editor={editor} />
