4f842d5cf81e3dbbfca53dc9927fc2d98aa4e98c
Bruno Zamorano Alves Monteiro <bzam1204@gmail.com>
2024-08-22 07:28:07 -0400

    refactor: Improve flip card styles in FlashCardReadOnlyTemplate

--- app/ui/question-read-only-templates/flashcard-read-only-template.tsx
+++ app/ui/question-read-only-templates/flashcard-read-only-template.tsx
@@ -45,30 +45,30 @@ const FlashCardReadOnlyTemplate = ({ question }: { question: Question }) => {
     });
 
     const flipCardTailwind = clsx(
-        "rounded-md p-4 cursor-pointer w-full h-full absolute bg-gray-200",
+        "rounded-md p-4 cursor-pointer w-full h-full absolute bg-gray-200 ",
     )
 
     return (
-        <div className="flex items-center justify-center  w-full "
+        <div className="flex items-center justify-center  w-full gap-8 "
             onMouseEnter={() => setShowDeleteIcon(true)}
             onMouseLeave={() => setShowDeleteIcon(false)}>
             <div className='grow'></div>
-            <div className="flip-card  flex justify-center items-center w-full max-w-screen-sm h-[25rem] drop-shadow-sm " onClick={handleFlip}>
+            <div className="flip-card   flex justify-center items-center w-full max-w-screen-sm h-[25rem] drop-shadow-sm " onClick={handleFlip}>
 
                 <motion.div
                     initial={false}
                     className=" flip-card-inner w-full h-full"
                     animate={{ rotateY: isFlipped ? 180 : 360 }}
-                    transition={{ duration: 0.6, animationDirection: 'normal' }}
+                    transition={{ duration: 0.2, animationDirection: 'normal' }}
                     onAnimationComplete={() => setIsAnimating(false)}
                 >
-                    <div className={flipCardTailwind + ' flip-card-front '}>
+                    <div className={flipCardTailwind + ' flip-card-front hover:bg-amber-300 transition-all'}>
                         <EditorContent content={question.body} editor={frontEditor} />
                     </div>
 
 
                     <div className={flipCardTailwind + ' flip-card-back  overflow-y-auto default-scrollbar'}>
-                        <EditorContent content={question.explanation} editor={backEditor} />
+                        <EditorContent content={question.explanation ?? ""} editor={backEditor} />
                     </div>
                 </motion.div>
                 <div className='grow'></div>
